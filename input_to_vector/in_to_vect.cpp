d229073d0f68e0c3d6624fcd6a01bdca390b4811
Alexey Rozhin <alexey.rozhin90@gmail.com>
2014-09-01 23:34:01 +0400

    ~ evaluate temperature params

--- input_to_vector/in_to_vect.cpp
+++ input_to_vector/in_to_vect.cpp
@@ -8,6 +8,20 @@ int main()
 	{
 		temps.push_back(temp);
 	}
+	
+	double sum;
+	
+	for (int i = 0; i < temps.size(); ++i)
+	{
+		sum += temps[i];
+	}
+	
+	cout << "average temperature: " << sum / temps.size() << endl;
+	
+	
+	sort(temps.begin(), temps.end());
+	
+	cout << "mediana: " << temps[temps.size() / 2] << endl;
 
 	return 1;
 }
